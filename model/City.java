c973cc42874f1c10023b405f4bd7ebab5bf5a7e3
varunsarmab2205 <varunsarmab22@gmail.com>
2026-01-29 12:10:25 +0530

    Add files via upload

---
+++ model/City.java
@@ -0,0 +1,15 @@
+package com.var.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class City {
+    private int cityId;
+    private String name;
+}
