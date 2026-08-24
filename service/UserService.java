49d363f2450a87fdaf3d2407983128c5d7e5d258
VARUN KANDUKURI <varunsarmab22@gmail.com>
2026-08-24 14:39:50 +0530

    Reformat UserService.java with consistent spacing

--- service/UserService.java
+++ service/UserService.java
@@ -13,5 +13,5 @@ public interface UserService {
     boolean updatePassword(UpdatePasswordRequest request);
     UserResponse getUserById(int userId);
     List<UserResponse> getAllUsers();
-    boolean updateUserRole(int userId, String role);
+    boolean updateUserRole(int userId, String     role);
 }
